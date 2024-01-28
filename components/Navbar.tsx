2c9031152a211d1734b4e6c07bdf1a791c678c92
Xu Wang <xwang9092@gmail.com>
2024-01-27 21:01:44 -0500

    feat: format change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,12 @@ import { NAV_LINKS } from "@/constants/index";
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-        <Link href="/">
-            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
-            <ul className="hidden h-full gap-12 lg:flex">
-            </ul>
-        </Link>
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        <ul className="hidden h-full gap-12 lg:flex"></ul>
+      </Link>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
