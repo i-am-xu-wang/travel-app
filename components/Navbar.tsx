86541b59dfd8ca7a0c6383c0ec7bf342dcd3f626
Xu Wang <xwang9092@gmail.com>
2024-01-27 20:59:15 -0500

    feat: set up the project

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants/index";
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
+            <ul className="hidden h-full gap-12 lg:flex">
+            </ul>
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar;
